d9f48a4e24b01d68a732c9736a237719a11a77e0
ivanrozhd <rozhdestvenskii200301@gmail.com>
2025-05-03 18:48:47 +0200

    prompt2MedImage check

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
+import torch
+from diffusers import StableDiffusionPipeline
 
-if __name__ == '__main__':
-    print("Hello!")
+model_id = "Nihirc/Prompt2MedImage"
+device = "cuda"
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to(device)
+
+prompt = "Showing the subtrochanteric fracture in the porotic bone."
+image = pipe(prompt).images[0]  
+    
+image.save("porotic_bone_fracture.png")
