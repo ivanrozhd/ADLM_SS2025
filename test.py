e8a21a5a9a43ae7ac016ac23a7ad9da1710cdbfd
Ivan Rozhdestvenskii <70483604+ivanrozhd@users.noreply.github.com>
2025-05-21 21:01:19 +0200

    more examples with lungs

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ pipe = pipe.to(device)
 
 # Brain CT prompts with minimal variation
 prompts = [
-    "chest x-ray revealing  pneumonia",
-
+    "Chest x-ray of normal lungs and heart",
+    "chest x-ray indicating possible infection"
 ]
 
 fixed_seed = random.randint(0, 10000)
@@ -21,9 +21,9 @@ generator = torch.Generator(device=device).manual_seed(fixed_seed)
 #fixed_seed = random.randint(0, 10000)
 #generator = torch.Generator(device=device).manual_seed(fixed_seed)
 image1 = pipe(prompts[0], generator=generator).images[0]
-image1.save("img2_xray_pneumonia.png")
-#image2 = pipe(prompts[1], generator=generator).images[0]
-#image2.save("img2_xray_mass_right.png")
+image1.save("img2_xray_normal.png")
+image2 = pipe(prompts[1], generator=generator).images[0]
+image2.save("img2_xray_infection.png")
 
 
 
