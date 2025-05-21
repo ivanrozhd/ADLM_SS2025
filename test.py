7ec8e7eca5e357659e263565d122729911104090
Ivan Rozhdestvenskii <70483604+ivanrozhd@users.noreply.github.com>
2025-05-21 17:30:06 +0200

    generated xray images of chest

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ pipe = pipe.to(device)
 
 # Brain CT prompts with minimal variation
 prompts = [
-    "High resolution CT chest slices of mild bronchiectasis of the right lower lobe with partial collapse of right middle lobe",
-    "High resolution CT chest slices of mild bronchiectasis of the right lower lobe with total collapse of right middle lobe",
+    "chest x-ray revealing  pneumonia",
+
 ]
 
 fixed_seed = random.randint(0, 10000)
@@ -20,12 +20,11 @@ generator = torch.Generator(device=device).manual_seed(fixed_seed)
 
 #fixed_seed = random.randint(0, 10000)
 #generator = torch.Generator(device=device).manual_seed(fixed_seed)
-#image1 = pipe(prompts[0], generator=generator).images[0]
-#image1.save("img1_fix_lungs_healthy.png")
-image2 = pipe(prompts[0], generator=generator).images[0]
-image2.save("img2_fix_lungs_partially.png")
-image3 = pipe(prompts[1], generator=generator).images[0]
-image3.save("img3_fix_lungs_fully.png")
+image1 = pipe(prompts[0], generator=generator).images[0]
+image1.save("img2_xray_pneumonia.png")
+#image2 = pipe(prompts[1], generator=generator).images[0]
+#image2.save("img2_xray_mass_right.png")
+
 
 
 
