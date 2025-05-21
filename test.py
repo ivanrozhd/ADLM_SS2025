c12d336c0fd8e97320d69a43d4638e47a0548415
Ivan Rozhdestvenskii <70483604+ivanrozhd@users.noreply.github.com>
2025-05-21 11:02:52 +0200

    image generation for the first presi

--- test.py
+++ test.py
@@ -11,16 +11,21 @@ pipe = pipe.to(device)
 
 # Brain CT prompts with minimal variation
 prompts = [
-    "CT scan of the lungs showing a tumor nodule", # slightly different
+    "High resolution CT chest slices of mild bronchiectasis of the right lower lobe with partial collapse of right middle lobe",
+    "High resolution CT chest slices of mild bronchiectasis of the right lower lobe with total collapse of right middle lobe",
 ]
 
-fixed_seed = 1311 #random.randint(0, 10000)
+fixed_seed = random.randint(0, 10000)
 generator = torch.Generator(device=device).manual_seed(fixed_seed)
 
 #fixed_seed = random.randint(0, 10000)
 #generator = torch.Generator(device=device).manual_seed(fixed_seed)
+#image1 = pipe(prompts[0], generator=generator).images[0]
+#image1.save("img1_fix_lungs_healthy.png")
 image2 = pipe(prompts[0], generator=generator).images[0]
-image2.save("img2_fix_lungs.png")
+image2.save("img2_fix_lungs_partially.png")
+image3 = pipe(prompts[1], generator=generator).images[0]
+image3.save("img3_fix_lungs_fully.png")
 
 
 
