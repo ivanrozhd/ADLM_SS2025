f049d52474b4048824d394a61e194e0018edf834
Ivan Rozhdestvenskii <rozhdest@selene.aim.cit.tum.de>
2025-05-28 15:47:44 +0200

    a few generated images

--- prompt-to-prompt-Stablediffusion/ivan_test.py
+++ prompt-to-prompt-Stablediffusion/ivan_test.py
@@ -1,7 +1,7 @@
 from diffusers import StableDiffusionPipeline
 import torch
 import cross_attention_editting
-
+import random
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 ldm_stable = StableDiffusionPipeline.from_pretrained("Nihirc/Prompt2MedImage").to(device) #torch_dtype=torch.float16)
 tokenizer = ldm_stable.tokenizer
@@ -20,9 +20,10 @@ Return:
     x_t : torch.Size([1, 4, 64, 64]) is a single noise seed sampled from a standard normal distribution -> independent of the prompts
     image : (amount of prompts, 512, 512, 3) 
 """
-prompts = ["chest x-ray showing a large tumor", "chest x-ray showing a small tumor"]
+prompts = ["chest x-ray of 6-year-old child", "chest x-ray of 26-year-old", "chest x-ray of 1-year-old child"]
 displayNumber = 0
-g_cpu = torch.Generator().manual_seed(8888) #specify a seed for the initial image
+fixed_seed = random.randint(0, 10000)
+g_cpu = torch.Generator().manual_seed(fixed_seed) #specify a seed for the initial image
 controller = cross_attention_editting.AttentionStore()
 image, x_t = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
                                                 NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
@@ -31,3 +32,5 @@ displayNumber +=1
 cross_attention_editting.show_cross_attention(tokenizer, prompts, displayNumber, controller, res=16, from_where=("up", "down"), select=0) # select define which prompt
 displayNumber +=1
 cross_attention_editting.show_cross_attention(tokenizer, prompts, displayNumber, controller, res=16, from_where=("up", "down"), select=1)
+displayNumber +=1
+cross_attention_editting.show_cross_attention(tokenizer, prompts, displayNumber, controller, res=16, from_where=("up", "down"), select=2)
