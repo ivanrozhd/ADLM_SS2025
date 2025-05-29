8646f5e10946d891553f9cd100c8a029840e5292
Ivan Rozhdestvenskii <70483604+ivanrozhd@users.noreply.github.com>
2025-05-29 18:03:46 +0200

    small changes

--- prompt-to-prompt-Stablediffusion/ivan_test.py
+++ prompt-to-prompt-Stablediffusion/ivan_test.py
@@ -25,54 +25,21 @@ Return:
 #prompts = ["Chest X-ray demonstrating a pleural effusion"]
 displayNumber = 0
 fixed_seed = random.randint(0, 10000)
-g_cpu = torch.Generator().manual_seed(8888)
+g_cpu = torch.Generator().manual_seed(fixed_seed)
 #g_cpu = torch.Generator().manual_seed(8888) #specify a seed for the initial image
 
 
-
-#controller = cross_attention_editting.AttentionStore()
-#_ , x_t = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
-                                             #  NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
-                                             #  latent=None, run_baseline=False, generator=g_cpu)
-#displayNumber +=1
-#cross_attention_editting.show_cross_attention(tokenizer, prompts, displayNumber, controller, res=16, from_where=("up", "down"), select=0) # select define which prompt
-#displayNumber +=1
-#cross_attention_editting.show_cross_attention(tokenizer, prompts, displayNumber, controller, res=16, from_where=("up", "down"), select=1)
-
-
-""" AttentionRefine: add words """
-#prompts = ["Computed tomography of the chest",
-#           "Computed tomography of the chest showing bilateral lung opacities"]
-
-#prompts = ["Computed tomography of the chest",
-# "Computed tomography of the chest showing bilateral lung opacities"]
-
-prompts = [ "Computed tomography of the chest with pleural effusion", "Computed tomography of the normal chest"]
-
-lb = cross_attention_editting.LocalBlend(prompts, ("chest", "chest"), MAX_NUM_WORDS=MAX_NUM_WORDS, tokenizer=tokenizer, device=device)
-controller = cross_attention_editting.AttentionRefine(prompts, num_steps=NUM_DIFFUSION_STEPS, tokenizer=tokenizer, device=device,
-                                                  cross_replace_steps=.4, self_replace_steps=.4, local_blend=lb)
-displayNumber += 1
-_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
-                                          NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
-                                         latent=None, run_baseline=True,  generator=g_cpu)
+prompts = ["Chest computed tomography reveals lung nodules"]
+displayNumber = 0
+g_cpu = torch.Generator().manual_seed(1234) #specify a seed for the initial image
+controller = cross_attention_editting.AttentionStore()
+image, x_t = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
+                                                NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
+                                                latent=None, run_baseline=False, generator=g_cpu)
+displayNumber +=1
+cross_attention_editting.show_cross_attention(tokenizer, prompts, displayNumber, controller, res=16, from_where=("up", "down"), select=0) # select define which prompt
 
 
-lb = cross_attention_editting.LocalBlend(prompts, ("chest", "chest"), MAX_NUM_WORDS=MAX_NUM_WORDS, tokenizer=tokenizer, device=device)
-controller = cross_attention_editting.AttentionRefine(prompts, num_steps=NUM_DIFFUSION_STEPS, tokenizer=tokenizer, device=device,
-                                                  cross_replace_steps=.2, self_replace_steps=.4, local_blend=lb)
-displayNumber += 1
-_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
-                                          NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
-                                         latent=None, run_baseline=True,  generator=g_cpu)
-
-lb = cross_attention_editting.LocalBlend(prompts, ("chest", "chest"), MAX_NUM_WORDS=MAX_NUM_WORDS, tokenizer=tokenizer, device=device)
-controller = cross_attention_editting.AttentionRefine(prompts, num_steps=NUM_DIFFUSION_STEPS, tokenizer=tokenizer, device=device,
-                                                  cross_replace_steps=.9, self_replace_steps=.4, local_blend=lb)
-displayNumber += 1
-_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
-                                          NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
-                                         latent=None, run_baseline=True,  generator=g_cpu)
 
 
 """ AttentionRefine: swap words """
@@ -89,3 +56,42 @@ _ = cross_attention_editting.run_and_display(prompts, displayNumber, controller,
 #_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
 #                                            NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
 #                                            latent=None, run_baseline=False, generator=g_cpu)
+
+prompts = ["chest x-ray of 26-year-old",  "chest x-ray of 26-year-old indicating infection"]
+
+
+controller = cross_attention_editting.AttentionRefine(prompts, num_steps=NUM_DIFFUSION_STEPS, tokenizer=tokenizer, device=device,
+                                                  cross_replace_steps=.8, self_replace_steps=.4)
+displayNumber += 1
+_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
+                                          NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
+                                         latent=x_t, run_baseline=True)
+
+prompts = ["chest x-ray of 26-year-old",  "chest x-ray of 26-year-old where right lung is bigger than left lung"]
+
+controller = cross_attention_editting.AttentionRefine(prompts, num_steps=NUM_DIFFUSION_STEPS, tokenizer=tokenizer, device=device,
+                                                  cross_replace_steps=.8, self_replace_steps=.4)
+displayNumber += 1
+_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
+                                          NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
+                                         latent=x_t, run_baseline=True)
+
+
+prompts = ["chest x-ray of 26-year-old",  "chest x-ray of 26-year-old demonstrating an abnormal heart"]
+
+controller = cross_attention_editting.AttentionRefine(prompts, num_steps=NUM_DIFFUSION_STEPS, tokenizer=tokenizer, device=device,
+                                                  cross_replace_steps=.8, self_replace_steps=.4)
+displayNumber += 1
+_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
+                                          NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
+                                         latent=x_t, run_baseline=True)
+
+
+prompts = ["chest x-ray of 26-year-old",  "chest x-ray of 26-year-old showing a mass in the upper lobe"]
+
+controller = cross_attention_editting.AttentionRefine(prompts, num_steps=NUM_DIFFUSION_STEPS, tokenizer=tokenizer, device=device,
+                                                  cross_replace_steps=.8, self_replace_steps=.4)
+displayNumber += 1
+_ = cross_attention_editting.run_and_display(prompts, displayNumber, controller, ldm_stable,
+                                          NUM_DIFFUSION_STEPS=NUM_DIFFUSION_STEPS, GUIDANCE_SCALE=GUIDANCE_SCALE,
+                                         latent=x_t, run_baseline=True)
